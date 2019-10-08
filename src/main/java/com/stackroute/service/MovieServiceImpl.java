1e7dcd6da3c9a8ec78b1aa6859d500f049687bae
antara.hembram <antara.hembram@cgi.com>
2019-10-08 18:04:26 +0530

    changes in pom

--- src/main/java/com/stackroute/service/MovieServiceImpl.java
+++ src/main/java/com/stackroute/service/MovieServiceImpl.java
@@ -11,7 +11,7 @@ import java.util.List;
 public class MovieServiceImpl implements MovieService {
 
     @Autowired
-    MovieRepository movieRepository;
+    private MovieRepository movieRepository;
 
     public MovieServiceImpl(MovieRepository movieRepository) {
         this.movieRepository = movieRepository;
