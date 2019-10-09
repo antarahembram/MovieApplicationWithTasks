da2211898853b58c09b05f33d3f28a23f581e7be
antara.hembram <antara.hembram@cgi.com>
2019-10-09 14:33:07 +0530

    changes in service

--- src/main/java/com/stackroute/MovieApplication.java
+++ src/main/java/com/stackroute/MovieApplication.java
@@ -3,16 +3,14 @@ package com.stackroute;
 //import com.stackroute.seeder.DataSeeder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-public class MovieapplicationApplication {
+public class MovieApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MovieapplicationApplication.class, args);
+		SpringApplication.run(MovieApplication.class, args);
 	}
 
 
